41d1876d4e706f01ecf2394add6730f6e320a1da
Thomas <mcarothomas@gmail.com>
2013-12-11 21:41:04 +0100

    Test pour voir si tout marche depuis eclipse =)  - Ajout de la javadoc pour fr.istic.evc.factory.Factory.java

--- src/fr/istic/evc/factory/Factory.java
+++ src/fr/istic/evc/factory/Factory.java
@@ -3,6 +3,11 @@ package fr.istic.evc.factory;
 import fr.istic.evc.object.common.controller.Univers;
 import fr.istic.evc.object.common.controller.World;
 
+/**
+ * Singleton - Factory permettant de créer le monde et l'univers
+ * @author Kevin Ramage - Thomas Caro - M2GL
+ *
+ */
 public class Factory {
 	
 	private static Factory instance;
